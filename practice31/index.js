9bf7b06072d8c953b868f1c34c8b9c7387f8c005
Lou Pruitt <Lpruitt2004@gmail.com>
2018-07-27 11:02:58 -0700

    completed mathCombo

---
+++ practice31/index.js
@@ -0,0 +1,10 @@
+function cocococombomath(num1, num2, num3, num4, num5) {
+    var result1 = num1 * num2;
+    var result2 = result1 + num3;
+    var result3 = result2 / num4;
+    var result4 = result3 - num5;
+    
+    return result4;
+}
+
+console.log(cocococombomath( 3, 2, 6, 3, 5 ));
